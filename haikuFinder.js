a5b885e0c20ed5dcaf4ceacb996a9ce2798bb95d
Travis Ryan <traryan@proofpoint.com>
2018-08-01 10:40:45 -0400

    haiku finder works

--- haikuFinder.js
+++ haikuFinder.js
@@ -1,21 +1,45 @@
-const rp = require("request-promise");
 const request = require("request");
-const q = require("q");
-
-var phrases = ["hello i am a phrase", "this is a phrase", "cat", "hello hello hello"];
-var words = ["cat", "balloon", "gubernatorial", "hello", "yes", "syllables", "dictatorship"];
 
 function main(){
-    var originalPhrases = phrases;
-    var splitSentences = splitText(phrases);
-    console.log(splitSentences);
-    for(var i = 0; i < originalPhrases.length; i++){
-        console.log("SENTENCE: "+originalPhrases[i]);
+    const phrases = ["hello i am a sentence. There are many words in this sentence.", "seven syllables right here", "this is a phrase", "cat", "hello hello hello", "five syllables here", "seven syllables here dawg", "five more syllables"];
+    shuffleArray(phrases);
+    var fiveSyllablePhrases = [];
+    var sevenSyllablePhrases = [];
+    var phrasesRemaining = phrases.length;
+    for(var i = 0; i < phrases.length; i++){
+        getTotalSyllablesForArrayOfWords(phrases[i], function(phrase, totalSyllables){
+            phrasesRemaining--;
+            if(totalSyllables==5)
+                fiveSyllablePhrases.push(phrase);
+            else if(totalSyllables==7)
+                sevenSyllablePhrases.push(phrase);
+            if(phrasesRemaining<=0){
+                printHaiku(fiveSyllablePhrases, sevenSyllablePhrases);
+            }
+        });
     }
 }
 
 main();
 
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function printHaiku(fiveSyllablePhrases, sevenSyllablePhrases){
+    console.log("\n\n\n");
+    console.log(fiveSyllablePhrases[0]);
+    console.log(sevenSyllablePhrases[0]);
+    console.log(fiveSyllablePhrases[1]);
+    console.log("\n\n\n");
+}
+
+// turns a string into a space-delimeted string array with only alphabetic characters
 function splitText(phrases){
     for(var i = 0; i < phrases.length; i++){
         phrases[i].replace(/[^a-zA-Z]/gi, '')
@@ -24,38 +48,21 @@ function splitText(phrases){
     return phrases;
 }
 
-function performRequests(words) {
+function getTotalSyllablesForArrayOfWords(phrase, callback) {
+    var words = phrase.split(" ");
     for (var i = 0; i < words.length; i++) {
-        var returnedData = [];
+        var totalSyllables = 0;
         var requestsRemaining = words.length;
-        request('https://api.datamuse.com/words?sp=' + words[i] + '&md=s', function (error, response, body) {
+        request('https://api.datamuse.com/words?sp=' + words[i].toLowerCase() + '&md=s', function (error, response, body) {
             if (error)
                 console.log('Error:', error);
             else {
-                returnedData.push(body);
+                totalSyllables+=JSON.parse(body)[0]["numSyllables"];
                 --requestsRemaining;
                 if (requestsRemaining <= 0) {
-                    var simpleResults = simplifyResults(returnedData);
-                    printSyllables(simpleResults);
+                    callback(phrase, totalSyllables);
                 }
             }
         });
-    }
-}
-
-function simplifyResults(data) {
-    var simpleResults = [];
-    for (var i = 0; i < data.length; i++) {
-        simpleResults.push({ "word": JSON.parse(data[i])[0]["word"], "numSyllables": JSON.parse(data[i])[0]["numSyllables"] });
-    }
-    simpleResults.sort(function (a, b) {
-        return a.numSyllables > b.numSyllables;
-    });
-    return simpleResults;
-}
-
-function printSyllables(data) {
-    for (i = 0; i < data.length; i++) {
-        console.log(data[i]["word"] + ", " + data[i]["numSyllables"]);
     }
 }
